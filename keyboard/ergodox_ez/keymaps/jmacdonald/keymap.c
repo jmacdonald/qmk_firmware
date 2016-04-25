ae0e91b24a2afbd05c8a1ba8a18fb61883822619
Jordan MacDonald <jordan@wastedintelligence.com>
2016-04-24 21:58:54 -0400

    Add explicit brace/bracket keys. Remap function keys.

--- keyboard/ergodox_ez/keymaps/jmacdonald/keymap.c
+++ keyboard/ergodox_ez/keymaps/jmacdonald/keymap.c
@@ -10,21 +10,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |      |           |      |   6  |   7  |   8  |   9  |   0  |  Bkspc |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |Ply/Ps|           |  6   |   7  |   8  |   9  |   0  |   =  |  Bkspc |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  F1  |           |  F3  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |   {  |           |   }  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Esc    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Retn  |
- * |--------+------+------+------+------+------|      |           |  F4  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|   [  |           |   ]  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |LCtrl |      |      | Left | Right|                                       |  Up  | Down |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      |Ply/Ps|       | Vol- | Vol+ |
+ *                                        |  F1  |  F2  |       |  F3  |  F4  |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      |      |       |  F7  |        |      |
- *                                 | '/Cmd|  _   |------|       |------|    -   |Space |
+ *                                 |      |      | Vol- |       | Vol+ |        |      |
+ *                                 | '/Cmd|   -  |------|       |------|    _   |Space |
  *                                 |      |      | LAlt |       |  =   |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -32,22 +32,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_NO,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_F1,
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MPLY,
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LCBR,
         KC_ESC,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_NO,
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTL,        KC_NO,        KC_NO,  KC_LEFT,KC_RGHT,
-                                                      KC_NO,  KC_MPLY,
-                                                              KC_NO,
+                                                      KC_F1,  KC_F2,
+                                                              KC_VOLD,
                                MT(MOD_LGUI, KC_QUOT), KC_MINS,KC_LALT,
         // right hand
-        KC_NO,       KC_6,   KC_7,   KC_8,   KC_9,   KC_0,    KC_BSPC,
-        KC_F3,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
+        KC_6,        KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,  KC_BSPC,
+        KC_RCBR,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENTER,
-        KC_F4,       KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
+        KC_RBRC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
                              KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC, KC_NO,
-        KC_VOLD,     KC_VOLU,
-        KC_F7,
+        KC_F3,       KC_F4,
+        KC_VOLU,
         KC_EQL,      KC_UNDS,KC_SPC
     ),
 };
