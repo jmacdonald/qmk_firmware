954881aab259b9de3533dd13f4180532c8dc9cda
Jordan MacDonald <jordan@wastedintelligence.com>
2016-09-26 09:56:28 -0400

    Shift numbers back to original locations

--- keyboard/ergodox_ez/keymaps/jmacdonald/keymap.c
+++ keyboard/ergodox_ez/keymaps/jmacdonald/keymap.c
@@ -9,7 +9,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |Ply/Ps|           |  6   |   7  |   8  |   9  |   0  |   =  |  Bkspc |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   6  |           |   7  |   8  |   9  |   0  |   -  |   =  |  Bkspc |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |   {  |           |   }  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *   |LCtrl | LAlt |      | Left | Right|                                       |  Up  | Down |   [  |   ]  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |  F1  |  F2  |       |  F3  |  F4  |
+ *                                        |  F1  |Ply/Ps|       |  F3  |  F4  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Vol- |       | Vol+ |        |      |
  *                                 | '/Cmd|   -  |------|       |------|    _   |Spc/L1|
@@ -31,16 +31,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_MPLY,
+        KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_LCBR,
         KC_ESC,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LBRC,
         KC_LCTL,        KC_LALT,      KC_NO,  KC_LEFT,KC_RGHT,
-                                                      KC_F1,  KC_F2,
+                                                      KC_F1,  KC_MPLY,
                                                               KC_VOLD,
                                MT(MOD_LGUI, KC_QUOT), KC_MINS,KC_LALT,
         // right hand
-        KC_6,        KC_7,   KC_8,   KC_9,   KC_0,   KC_EQL,  KC_BSPC,
+        KC_7,        KC_8,   KC_9,   KC_0,   KC_MINS,KC_EQL,  KC_BSPC,
         KC_RCBR,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,    KC_BSLS,
                      KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN, KC_ENTER,
         KC_RBRC,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH, KC_RSFT,
